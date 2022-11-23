aa02e958d20eba7f268622503e17e0b2ab9d69f6
Md Shadab <gabuyama68@gmail.com>
2022-11-23 16:29:58 +0530

    wip

--- src/containers/Home/Articles.jsx
+++ src/containers/Home/Articles.jsx
@@ -6,19 +6,20 @@ import ArticleCards from "../../components/cards/articles/ArticlesCard.jsx";
 import home from "./home.module.css";
 
 const Articles = () => {
+  let width = window.innerWidth;
   return (
     <div className={home.articlesContainer}>
       <h1>Our Latest Articles</h1>
       <p>Written by our experts</p>
       <Grid container spacing={2} justifyContent="space-around">
-        <Grid item xs={12} sm={12} md={4} lg={4}>
+        <Grid item xs={12} md={4} lg={4}>
           <ArticleCards
             main={true}
             title="Regular Exercise"
             des="Regular exercise can take away more the 80% of diseases both mental and physical"
           />
         </Grid>
-        <Grid item xs={12} sm={12} md={7} lg={6}>
+        <Grid item xs={12} md={7} lg={6}>
           <ArticleCards
             main={false}
             title="Regular Exercise"
