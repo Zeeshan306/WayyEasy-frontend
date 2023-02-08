04b977a6e00cd005770cfec7b2bfd4627a71f6bb
shadab <gabuyama68@gmail.com>
2023-02-08 12:41:02 +0530

    wip

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,50 +1,44 @@
-// import { Grid } from "@material-ui/core";
-// import React from "react";
+import { Grid } from "@material-ui/core";
+import React from "react";
 
-// import logo from "../../files/Images/icon.png";
-// import fb from "../../files/Images/fb.png";
-// import insta from "../../files/Images/insta.png";
-// import linkedIn from "../../files/Images/linkedIn.png";
-// import foot from "./footer.module.css";
-
-// const Footer = () => {
-//   return (
-//     <Grid
-//       container
-//       justifyContent="space-around"
-//       className={foot.mainContainer}
-//     >
-//       <div className={foot.section1}>
-//         <img src={logo} alt="footer-logo" />
-//         <h4>Our vision</h4>
-//         <p>The world without fear and independent from all diseases.</p>
-//       </div>
-//       <div className={foot.section2}>
-//         <a href="#">WayyEasy Official Blog</a>
-//         <a href="#">WayyEasy Official Circle</a>
-//         <a href="/privacy">Privacy Policy</a>
-//         <a href="/terms$conditions">Terms and Conditions</a>
-//       </div>
-//       <div className={foot.section3}>
-//         <h3>Follow Us</h3>
-//         <a href="https://www.facebook.com/WayyEasy" target="_blank">
-//           <img src={fb} alt="social-media" />
-//         </a>
-//         <a href="https://www.linkedin.com/company/wayyeasy/" target="_blank">
-//           <img src={linkedIn} alt="social-media" />
-//         </a>
-//         <a href="https://www.instagram.com/wayyeasy/" target="_blank">
-//           <img style={{ width: "32px" }} src={insta} alt="social-media" />
-//         </a>
-//       </div>
-//     </Grid>
-//   );
-// };
-
-// export default Footer;
+import logo from "../../files/Images/icon.png";
+import fb from "../../files/Images/fb.png";
+import insta from "../../files/Images/insta.png";
+import linkedIn from "../../files/Images/linkedIn.png";
+import foot from "./footer.module.css";
 
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <Grid
+      container
+      justifyContent="space-around"
+      className={foot.mainContainer}
+    >
+      <div className={foot.section1}>
+        <img src={logo} alt="footer-logo" />
+        <h4>Our vision</h4>
+        <p>The world without fear and independent from all diseases.</p>
+      </div>
+      <div className={foot.section2}>
+        <a href="#">WayyEasy Official Blog</a>
+        <a href="#">WayyEasy Official Circle</a>
+        <a href="/privacy">Privacy Policy</a>
+        <a href="/terms$conditions">Terms and Conditions</a>
+      </div>
+      <div className={foot.section3}>
+        <h3>Follow Us</h3>
+        <a href="https://www.facebook.com/WayyEasy" target="_blank">
+          <img src={fb} alt="social-media" />
+        </a>
+        <a href="https://www.linkedin.com/company/wayyeasy/" target="_blank">
+          <img src={linkedIn} alt="social-media" />
+        </a>
+        <a href="https://www.instagram.com/wayyeasy/" target="_blank">
+          <img style={{ width: "32px" }} src={insta} alt="social-media" />
+        </a>
+      </div>
+    </Grid>
+  );
 };
 
 export default Footer;
